86f1f33a4319b0de059060502410774f8521e6e0
Moreno Antunes <morenomdz@gmail.com>
2019-04-28 10:48:30 -0300

    clean up

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 import { getToken } from './auth';
 
 const api = axios.create({
-  baseURL: 'https://134.209.63.4:3030',
+  // baseURL: 'https://madz.tech',
+  // baseURL: 'https://134.209.63.4',
   // baseURL: 'http://localhost:3333/',
-  // baseURL: 'https://meetapp-morenomdz.herokuapp.com/',
+  baseURL: 'https://meetapp-morenomdz.herokuapp.com/',
 });
 
 api.interceptors.request.use(async (config) => {
